9db108f5ce462025b775995cfa98c34994d5e35b
Tomas <tomasalibertiquinto.sociales.ma@gmail.com>
2026-08-24 14:54:27 -0300

    feat: configuracion de VITE_API_URL en el frontend"

--- frontend/src/api/auth.api.ts
+++ frontend/src/api/auth.api.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
-// Quitamos el "/api" para alinearlo directamente con NestJS
-const API_URL = "http://localhost:3000/auth";
 
+const API_URL =import.meta.env.VITE_API_URL || 'http://localhost:3000';
 export interface UserProfileDTO {
   id: number;
   nombre: string;
